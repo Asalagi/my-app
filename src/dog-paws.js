07b490bf27b2d34cfcb1066616e8b78500646435
Asalagi <mypaintedfrog@yahoo.com>
2023-05-03 15:46:30 -0500

    modifying adding navbar

--- src/dog-paws.js
+++ src/dog-paws.js
@@ -1,12 +1,24 @@
 import './dog-paws-css.css';
+import logo from './dog.png';
 
 function DogPaws() {
     return (
         <div>
             <div className="first-header">
                 <h1 className="header-add">
-                    Book now and receive free blueberry facial
+                    Book before June 1st and receive free blueberry facial
                 </h1>
+            </div>
+            <div clsssName="nav-bar">
+                <ul className="nav-bar">
+                   <div className="logo">
+                        <img src={logo} alt='' />
+                    </div>
+                    <li><a href="#about">About Us</a></li>
+                    <li><a href="#education">Education</a></li>
+                    <li><a href="#shop">Shop</a></li>
+                    <li><a href="#contact">Contact Us</a></li>
+                </ul>
             </div>
         </div>
     )
