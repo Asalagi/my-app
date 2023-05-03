66cb1db9705731614543ba3a61fa1a08c2f565af
Asalagi <mypaintedfrog@yahoo.com>
2023-05-03 15:12:29 -0500

    dog paws, layout, logo, css

---
+++ src/dog-paws.js
@@ -0,0 +1,16 @@
+import './dog-paws-css.css';
+
+function DogPaws() {
+    return (
+        <div>
+            <div className="first-header">
+                <h1 className="header-add">
+                    Book now and receive free blueberry facial
+                </h1>
+            </div>
+        </div>
+    )
+
+}
+
+export default DogPaws;
