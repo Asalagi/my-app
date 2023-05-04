7a5957c3e6fea572dabcaa99d60b6c745b89312e
Asalagi <mypaintedfrog@yahoo.com>
2023-05-04 15:52:05 -0500

    adding navigationbar component

---
+++ src/pages/my-layout-nav.js
@@ -0,0 +1,25 @@
+import './layout.css';
+import logo from './three-running-horses.png';
+
+function NavigationBar() {
+    return (
+        <div>
+            <nav className="navbar background">
+                <ul className="nav-list">
+                    <div className="logo">
+                        <img src={logo} alt='' />
+                    </div>
+                    <li><a href="#theteam">The Team</a></li>
+                    <li><a href="#facilities">Facilities</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#stallions">Stallions</a></li>
+                    <li><a href="#mares">Mares</a></li>
+                    <li><a href="#forsale">For Sale</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default NavigationBar;
--- src/pages/my-layout.js
+++ src/pages/my-layout.js
@@ -1,25 +1,14 @@
 import './layout.css';
 import logo from './three-running-horses.png';
 import farm from './horse-ranch.jpeg';
+import NavigationBar from './my-layout-nav'
 
 function Layout() {
     return (
         <div>
-            <nav className="navbar background">
-                <ul className="nav-list">
-                    <div className="logo">
-                        <img src={logo} alt='' />
-                    </div>
-                    <li><a href="#theteam">The Team</a></li>
-                    <li><a href="#facilities">Facilities</a></li>
-                    <li><a href="#services">Services</a></li>
-                    <li><a href="#stallions">Stallions</a></li>
-                    <li><a href="#mares">Mares</a></li>
-                    <li><a href="#forsale">For Sale</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                </ul>
-            </nav>
-
+           <div>
+            <NavigationBar />
+           </div>
           <section className="section">
             <div className="box-main">
                 <div className="firstHalf">
