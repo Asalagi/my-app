b1777fa9c1a56d6ec1159b93e0fb73ba763af91c
Asalagi <mypaintedfrog@yahoo.com>
2023-05-04 12:04:04 -0500

    moved things around, fixed my layout links

--- src/pages/my-layout.js
+++ src/pages/my-layout.js
@@ -1,4 +1,4 @@
-import './layout-css.css';
+import './layout.css';
 import logo from './three-running-horses.png';
 
 function Layout() {
@@ -9,17 +9,13 @@ function Layout() {
                     <div className="logo">
                         <img src={logo} alt='' />
                     </div>
-                    <li><a href="#theteam">The Team</a></li>
+                    <li><a href="#theteam">The&nbsp;Team</a></li>
                     <li><a href="#facilities">Facilities</a></li>
                     <li><a href="#stallions">Stallions</a></li>
                     <li><a href="#mares">Mares</a></li>
-                    <li><a href="#forsale">For Sale</a></li>
+                    <li><a href="#forsale">For&nbsp;Sale</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
-            <div className="rightNav">
-                <input type="text" name="search" id="search" />
-                <button className="btn btn-sm">Search</button>
-            </div>
             </nav>
 
           <section className="section">
@@ -29,18 +25,6 @@ function Layout() {
                     </h1>
                     <p className="text-small">
                         Put some text here maybe add a picture or something.
-                    </p>
-                </div>
-            </div>
-          </section>
-          <section className="section">
-            <div className="box-main">
-                <div className="secondHalf">
-                    <h1 className="text-big" id="program">
-                        Some more big text here
-                    </h1>
-                    <p className="text-small">
-                        some more text here add a picture or something.
                     </p>
                 </div>
             </div>
