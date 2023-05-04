7f4fe01e511b269dd30d739d6d5c4dd6150bafeb
Asalagi <mypaintedfrog@yahoo.com>
2023-05-04 16:12:06 -0500

    homepage component added and layout simplified

---
+++ src/pages/my-layout-homepage.js
@@ -0,0 +1,60 @@
+import './layout.css';
+import farm from "./horse-ranch.jpeg";
+
+function HomePage() {
+    return (
+        <section className="section">
+        <div className="box-main">
+            <div className="firstHalf">
+                <h1 className="text-big">
+                    Welcome to the ranch!
+                </h1>
+                <div className="image-container">
+                <img src={farm} alt='' />
+                </div>
+                <p className="text-small blah-blah">
+                    Put some text here maybe add a picture or something. 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+                    Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
+                </p>
+            </div>
+        </div>
+      </section>
+    )
+}
+
+export default HomePage;
--- src/pages/my-layout.js
+++ src/pages/my-layout.js
@@ -1,7 +1,7 @@
 import './layout.css';
-import farm from './horse-ranch.jpeg';
 import NavigationBar from './my-layout-nav';
 import LayoutFooter from './my-layout-footer';
+import HomePage from './my-layout-homepage';
 
 function Layout() {
     return (
@@ -9,57 +9,9 @@ function Layout() {
            <div>
             <NavigationBar />
            </div>
-          <section className="section">
-            <div className="box-main">
-                <div className="firstHalf">
-                    <h1 className="text-big">
-                        Welcome to the ranch!
-                    </h1>
-                    <div className="image-container">
-                    <img src={farm} alt='' />
-                    </div>
-                    <p className="text-small blah-blah">
-                        Put some text here maybe add a picture or something. 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
-                        Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
-                    </p>
-                </div>
-            </div>
-          </section>
+           <div>
+            <HomePage />
+           </div>
           <div>
             <LayoutFooter />
           </div>
